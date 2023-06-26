de1da0a8bc2a867c333ae22bd92329c88c0e28ea
symbatzinZinatova <symbat.zinat29@gmail.com>
2023-06-26 20:37:18 +0600

    path task done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,55 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 
 public class FileReader {
-
+    Profile profile = new Profile();
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (Stream<String> lines = Files.lines(Paths.get(file.toURI()))) {
+            return getProfileFromRawStrings(lines.toArray(String[]::new));
+        } catch (IOException e) {
+            throw new TestFileIncorrectException(e.getMessage());
+        }
     }
-}
+
+
+
+    public class TestFileIncorrectException extends RuntimeException {
+        TestFileIncorrectException(String message) {
+            super(message);
+        }
+
+        TestFileIncorrectException() {
+            super();
+        }
+    }
+
+    private Profile getProfileFromRawStrings(String[] strings) {
+
+            for (String str : strings) {
+                String[] answer = str.split(": ");
+                switch (answer[0]) {
+                    case "Name":
+                        profile.setName(answer[1]);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(answer[1]));
+                        break;
+                    case "Email":
+                        profile.setEmail(answer[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(answer[1]));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return profile;
+        }
+    }
